acf23e738dd50513ab02b45882e17d427f2ccf73
Ubuntu <ubuntu@ip-172-31-42-195.ap-south-1.compute.internal>
2026-04-12 03:41:39 +0000

    update from vps

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_ID = int(os.getenv("API_ID", "38116283"))
 API_HASH = os.getenv("API_HASH", "fb8638157056f21bdafc61e41e147f88")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = os.getenv("BOT_TOKEN","")
+BOT_TOKEN = os.getenv("BOT_TOKEN","8448335567:AAHzKcX7j_pTHivk4-u4u5r7AS48PAtl6hw")
 
 YOUTUBE_API_KEY = os.getenv("")
 # OpenAI Token
@@ -57,7 +57,7 @@ SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/PhilosSoul")
 SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/chattingzone0")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
 # Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
 AUTO_GCAST = os.getenv("AUTO_GCAST")
@@ -86,7 +86,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(os.getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session from @BRANDEDSTRINGSESSION_BOT on Telegram
-STRING1 = os.getenv("STRING_SESSION",  "")
+STRING1 = os.getenv("STRING_SESSION",  "AQJFm7sASzsPXhyiweX8HzXHsP_OsngbUXIkOofseWNcLwsKR72J-v588D4k1jQ9t4uY_R-iXQr0aNULByHrd9QA3QnFMTu76_pF0NVcwJAMpIaxqTqfZ4Td-s4k_kTuH4kFIWQXZQkNJROYU4eSPZsGraoBhqo60iQ2kBM56CdS3XG2tWnYt64sePY4J3YgZ9YysXe-QdQqgvJastPVidNtuEf9a11FB385mEBjm0HXJF7GSNgUqPsdhuEePJ_a3ehkgmBVwmcEzCZxthm5lu1F6rknoXMnlrL5WkALrF1ZlTEbM-K1Kr5lfwtYKfd4fM6UEs-VbTXoU3SPMZNWcfOFv0q_bgAAAAH_BpLpAA")
 STRING2 = os.getenv("STRING_SESSION2", None)
 STRING3 = os.getenv("STRING_SESSION3", None)
 STRING4 = os.getenv("STRING_SESSION4", None)
