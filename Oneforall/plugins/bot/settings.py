a21eae760f3cfa71082a82196aba73f25eca526a
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-23 03:39:15 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- Oneforall/plugins/bot/settings.py
+++ Oneforall/plugins/bot/settings.py
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
 from time import time
 
 from pyrogram import filters
