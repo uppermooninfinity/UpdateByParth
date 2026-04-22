1065c7b65b240d462faddb43b628cef8ad3b5f35
uppermooninfinity <uppermooninfinity@gmail.com>
2026-04-23 02:21:04 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- Oneforall/plugins/tools/speedtest.py
+++ Oneforall/plugins/tools/speedtest.py
@@ -1,45 +1,63 @@
 import asyncio
-
 import speedtest
+
 from pyrogram import filters
 from pyrogram.types import Message
 
 from Oneforall import app
 from Oneforall.misc import SUDOERS
-from Oneforall.utils.decorators.language import language
+
+SPEED_VIDEO = "https://graph.org/file/308891fd6e656bcc70f0e-272ff13d24d1820cf5.mp4"
 
 
-def testspeed(m, _):
-    try:
-        test = speedtest.Speedtest()
-        test.get_best_server()
-        m = m.edit_text(_["server_12"])
-        test.download()
-        m = m.edit_text(_["server_13"])
-        test.upload()
-        test.results.share()
-        result = test.results.dict()
-        m = m.edit_text(_["server_14"])
-    except Exception as e:
-        return m.edit_text(f"<code>{e}</code>")
-    return result
+def run_speedtest():
+    test = speedtest.Speedtest()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    return test.results.dict()
 
 
 @app.on_message(filters.command(["speedtest", "spt"]) & SUDOERS)
-@language
-async def speedtest_function(client, message: Message, _):
-    m = await message.reply_text(_["server_11"])
+async def speedtest_function(client, message: Message):
+
+    m = await message.reply_text("<blockquote>⚡ ʀᴜɴɴɪɴɢ ꜱᴘᴇᴇᴅ ᴛᴇꜱᴛ...</blockquote>")
+
     loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, testspeed, m, _)
-    output = _["server_15"].format(
-        result["client"]["isp"],
-        result["client"]["country"],
-        result["server"]["name"],
-        result["server"]["country"],
-        result["server"]["cc"],
-        result["server"]["sponsor"],
-        result["server"]["latency"],
-        result["ping"],
-    )
-    msg = await message.reply_photo(photo=result["share"], caption=output)
+
+    try:
+        result = await loop.run_in_executor(None, run_speedtest)
+    except Exception as e:
+        return await m.edit_text(f"<code>{e}</code>")
+
+    # Convert to Mbps
+    download = round(result["download"] / 1_000_000, 2)
+    upload = round(result["upload"] / 1_000_000, 2)
+
+    caption = f"""
+<blockquote>
+⚡ ꜱᴘᴇᴇᴅ ᴛᴇꜱᴛ ʀᴇꜱᴜʟᴛ ⚡
+
+📡 ɪꜱᴘ: {result['client']['isp']}
+🌍 ᴄᴏᴜɴᴛʀʏ: {result['client']['country']}
+
+🖥️ ꜱᴇʀᴠᴇʀ: {result['server']['name']}
+🏳️ ʟᴏᴄᴀᴛɪᴏɴ: {result['server']['country']} ({result['server']['cc']})
+🏢 ꜱᴘᴏɴꜱᴏʀ: {result['server']['sponsor']}
+
+📶 ᴘɪɴɢ: {result['ping']} ms
+⬇️ ᴅᴏᴡɴʟᴏᴀᴅ: {download} Mbps
+⬆️ ᴜᴘʟᴏᴀᴅ: {upload} Mbps
+</blockquote>
+"""
+    try:
+        await message.reply_video(
+            video=SPEED_VIDEO,
+            has_spoiler=True,
+            caption=caption,
+        )
+    except Exception:
+        await message.reply_text(caption)
+
     await m.delete()
